ca4bd2880ea63f648a576a28b634a7cc501d8a0b
ALJAZARIhadi <hadikezzhe@gmail.com>
2026-09-15 23:33:07 +0300

    Test icons confirmed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Test from './test'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Test/>
     </>
   )
 }
