e056e32a81cd29fbe182644b70007c2c629cacf3
ALJAZARIhadi <hadikezzhe@gmail.com>
2026-09-17 23:57:51 +0300

    trying to center the container

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-import TodoList from './TodoList'
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+import TodoList from "./TodoList";
 function App() {
-
-
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "green",
+      }}
+    >
+      <TodoList />
       {/* <Test/> */}
-      <TodoList/>
-    </>
-  )
+    </div>
+  );
 }
 
-
-export default App
+export default App;
